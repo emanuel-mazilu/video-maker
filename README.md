28db08a0ca1e4eb6152fcc699cd2c04c063e2fcc
Emanuel Mazilu <emanuel.mazilu@outlook.com>
2020-08-03 21:51:07 +0300

    changed variables names

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # video-maker
 universal translator, video maker from the translated text with speech
+
+REQUREMENTS:
+
+googletrans -- Google translate API
+gtts -- Google Text to Speech API
+moviepy -- video editing
+gizeh -- vector graphics
+mutagen -- handle audio metadata
--- python_video.py
+++ python_video.py
@@ -3,7 +3,6 @@ from gtts import gTTS
 
 import moviepy.editor as mpy
 import gizeh as gz
-from math import pi
 from mutagen.mp3 import MP3
 
 VIDEO_SIZE = (1280, 720)
@@ -13,14 +12,14 @@ WHITE = (255, 255, 255)
 WHITE_GIZEH = (1, 1, 1)
 BlACK = (0, 0, 0)
 
-traducator = Translator()
-expresiiArray = []
-FILENAME = "expresii.txt"
+translator = Translator()
+word_list = []
+FILENAME = "words.txt"
 
 
 with open(FILENAME) as f:
     for line in f:
-        expresiiArray.append(line.rstrip())
+        word_list.append(line.rstrip())
 
 initial_language = "en"
 translate_language = "ro"
@@ -28,26 +27,26 @@ translate_language = "ro"
 
 i = 1
 # For evrey individual line in file create a video with audio
-for text_netradus in expresiiArray:
+for translated_text in word_list:
     #translate text
-    tradus = traducator.translate(text_netradus, dest=translate_language)
-    print(text_netradus, " --> ", tradus.text)
-    text_tradus = tradus.text
+    translator_object = translator.translate(translated_text, dest=translate_language)
+    print(translated_text, " --> ", translator_object.text)
+    translated_text = translator_object.text
 
     #make audio files for the translated text
     #original text slow
-    myobj = gTTS(text=text_netradus, lang=initial_language, slow=True)
-    myobj.save("netradus" + ".mp3")
+    myobj = gTTS(text=translated_text, lang=initial_language, slow=True)
+    myobj.save("untranslated" + ".mp3")
     #tramslated text
-    myobj2 = gTTS(text=tradus.text, lang=translate_language, slow=False)
-    myobj2.save("tradus" + ".mp3")
+    myobj2 = gTTS(text=translator_object.text, lang=translate_language, slow=False)
+    myobj2.save("translated" + ".mp3")
     #original text
-    myobj3 = gTTS(text=text_netradus, lang=initial_language, slow=False)
-    myobj3.save("netradus_fast" + ".mp3")
+    myobj3 = gTTS(text=translated_text, lang=initial_language, slow=False)
+    myobj3.save("untranslated_fast" + ".mp3")
     #load audio files  
-    audio1 = MP3("netradus.mp3")
-    audio2 = MP3("tradus.mp3")
-    audio3 = MP3("netradus_fast.mp3")
+    audio1 = MP3("untranslated.mp3")
+    audio2 = MP3("translated.mp3")
+    audio3 = MP3("untranslated_fast.mp3")
 
     DURATION = audio1.info.length + audio2.info.length + audio3.info.length
     print(DURATION)
@@ -56,12 +55,12 @@ for text_netradus in expresiiArray:
         surface = gz.Surface(1280, 300, bg_color=WHITE_GIZEH)
         # add original text to screen
         text1 = gz.text(
-            text_netradus, fontfamily="Charter",
+            translated_text, fontfamily="Charter",
             fontsize=40, fontweight='bold', fill=BLUE, xy=(640, 40))
         text1.draw(surface)
         # add translated text to screen
         text2 = gz.text(
-            text_tradus, fontfamily="Charter",
+            translated_text, fontfamily="Charter",
             fontsize=40, fontweight='bold', fill=BLUE, xy=(640, 200))
         text2.draw(surface)
         return surface.get_npimage()
@@ -72,9 +71,9 @@ for text_netradus in expresiiArray:
     # load audio files to add them to video
     silence_half = mpy.AudioFileClip("0.5-second-of-silence.mp3")
     silence_sec = mpy.AudioFileClip("1-second-of-silence.mp3")
-    video_audio1 = mpy.AudioFileClip("netradus.mp3")
-    video_audio2 = mpy.AudioFileClip("tradus.mp3")
-    video_audio3 = mpy.AudioFileClip("netradus_fast.mp3")
+    video_audio1 = mpy.AudioFileClip("untranslated.mp3")
+    video_audio2 = mpy.AudioFileClip("translated.mp3")
+    video_audio3 = mpy.AudioFileClip("untranslated_fast.mp3")
 
     # put all sounds together
     video_audio = mpy.concatenate_audioclips([silence_half, video_audio1, silence_half, video_audio2, silence_half, video_audio3, silence_sec])
@@ -88,7 +87,7 @@ for text_netradus in expresiiArray:
 # combine all videos in one
 clips = []
 
-for i in range(1,len(expresiiArray)+1):
+for i in range(1,len(word_list)+1):
     clips.append(mpy.VideoFileClip("video-" + str(i) + ".mp4"))
 
 final_clip = mpy.concatenate_videoclips([*clips])
